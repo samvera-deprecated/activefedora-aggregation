f662f66d7156580ee5821d52f6bc1aa9103e6db9
Justin Coyne <justin@curationexperts.com>
2015-03-18 15:01:17 -0500

    Set the active_fedora environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_fedora/aggregation'
 require 'active_fedora/cleaner'
 require 'byebug' unless ENV['CI']
+ENV['environment'] = 'test'
 
 RSpec.configure do |config|
   config.before(:each) do
