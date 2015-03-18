f9c4defd56b6aa9e96ea1bcfbcc7815305d6e4ea
Justin Coyne <justin@curationexperts.com>
2015-03-18 09:49:55 -0500

    Initial commit

---
+++ lib/activefedora/aggregation.rb
@@ -0,0 +1,7 @@
+require "activefedora/aggregation/version"
+
+module Activefedora
+  module Aggregation
+    # Your code goes here...
+  end
+end
---
+++ lib/activefedora/aggregation/version.rb
@@ -0,0 +1,5 @@
+module Activefedora
+  module Aggregation
+    VERSION = "0.1.0"
+  end
+end
