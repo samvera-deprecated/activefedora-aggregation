771dc5b41f537b06f45acda3ddc5e4a64b6327df
Justin Coyne <justin@curationexperts.com>
2015-05-04 11:25:38 -0500

    A class name is not required for aggregates.

--- lib/active_fedora/aggregation/reflection.rb
+++ lib/active_fedora/aggregation/reflection.rb
@@ -4,6 +4,16 @@ module ActiveFedora::Aggregation
       ThroughAssociation
     end
 
+    def klass
+      klass = super
+      # This check precludes an inferred class like ::File from being used.
+      if klass.respond_to? :uri_to_id
+        klass
+      else
+        ActiveFedora::Base
+      end
+    end
+
     def collection?
       true
     end
