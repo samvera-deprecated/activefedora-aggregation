1a3c9b997256d7a505dc186d3c784d271bb7134c
Trey Terrell <trey.terrell@oregonstate.edu>
2015-05-05 10:14:33 -0700

    Add pry-byebug.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_fedora/aggregation'
 require 'active_fedora/cleaner'
-require 'byebug' unless ENV['CI']
+require 'pry' unless ENV['CI']
 ENV['environment'] = 'test'
 
 RSpec.configure do |config|
